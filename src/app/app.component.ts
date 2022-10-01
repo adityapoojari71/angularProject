a6163b985a26f6a5799c9b2395578ffa8d5502be
adityapoojari71 <adityapoojari71@gmail.com>
2022-10-01 16:31:22 +0530

    Code pushed to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const $:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-project';
+  userInforation:any =['Gina Williams', 'Jake Williams', 'Jamie John', 'John Doe', 'Jeff Stewart', 'Paula M. Keith'];
+  showUserInformation:boolean=false;
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    console.log(this.userInforation);
+  }
+
+  //method for get user Information
+  getUserInformation(event:any){
+    $('#story').css({'color':'','font-size':''});
+    if(event.target.value.includes('@')){
+      this.showUserInformation = true;
+    }
+    else{
+     this.showUserInformation = false;
+    }
+
+  }
+
+  //method for reset the textarea filed
+  clearUser(){
+    $('#story').val('');
+    this.showUserInformation = false;
+
+  }
+
+  highligtSelectedUser(){
+    $('#story').css({'color':'orange','font-size':'25px'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatAutocompleteModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
